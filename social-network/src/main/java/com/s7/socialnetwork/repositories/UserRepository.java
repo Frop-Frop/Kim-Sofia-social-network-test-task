eb1dc475ae67328be96b271dd65313a7fd58582b
User <sophianpilova@gmail.com>
2022-02-02 13:06:43 +0300

    search method added to UserRepository

--- social-network/src/main/java/com/s7/socialnetwork/repositories/UserRepository.java
+++ social-network/src/main/java/com/s7/socialnetwork/repositories/UserRepository.java
@@ -15,6 +15,12 @@ public interface UserRepository extends JpaRepository<RegularUser, Long> {
 
 	Optional<RegularUser> findByUsername(String username);
 
+	@Query(value = "select u.* from users u where lower(u.first_name) like lower(concat('%', :searchTerm, '%'))"
+			+ " or lower(u.last_name) like lower(concat('%', :searchTerm, '%'))"
+			+ " or lower(concat(u.first_name, ' ', u.last_name)) like lower(concat('%', :searchTerm, '%'))"
+			+ " or lower(concat(u.last_name, ' ', u.first_name)) like lower(concat('%', :searchTerm, '%'))", nativeQuery = true)
+	List<RegularUser> search(@Param("searchTerm") String searchTerm);
+
 	@Query(value = "SELECT u.* FROM users u JOIN friends f on u.id = f.user_id WHERE f.user_id=:id", nativeQuery = true)
 	List<RegularUser> findFriends(@Param("id") Long friendId);
 
