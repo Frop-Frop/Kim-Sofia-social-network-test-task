507403d4f6ba6ee8ff184325e1a5c53a381de84a
User <sophianpilova@gmail.com>
2022-02-03 15:59:28 +0300

    port number specified. javax.servlet dependency added

--- social-network/src/main/java/com/s7/socialnetwork/SocialNetworkApplication.java
+++ social-network/src/main/java/com/s7/socialnetwork/SocialNetworkApplication.java
@@ -2,6 +2,7 @@ package com.s7.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -10,7 +11,7 @@ import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(info = @Info(title = "Social network API", version = "1.0", description = "Made for S7 Travel Retail testTask", termsOfService = "https://www.apache.org/licenses/LICENSE-2.0", contact = @Contact(name = "Sofia Kim", email = "sophianpilova@gmail.com"), license = @License(name = "Apache license Version 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")))
 @SpringBootApplication
-public class SocialNetworkApplication {
+public class SocialNetworkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
